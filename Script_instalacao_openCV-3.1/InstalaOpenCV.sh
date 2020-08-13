cedb41fda8c8a1366609fff7208cc139b2f52f1d
FelipeMeganha <felipengmec@gmail.com>
2020-08-12 23:56:27 -0300

    Script Instala OpenCV

--- Script_instalacao_openCV-3.1/InstalaOpenCV.sh
+++ Script_instalacao_openCV-3.1/InstalaOpenCV.sh
@@ -38,7 +38,7 @@ echo "Download do codigo-fonte do OpenCV 3.1.0"
 echo "----------------------------------------"
 echo ""
 
-cd ~
+
 wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.1.0.zip
 unzip opencv.zip
 
@@ -60,9 +60,8 @@ echo "Compilacao do OpenCV 3.1.0"
 echo "--------------------------"
 echo ""
 
-cd ~/opencv-3.1.0/
-mkdir build
-cd build
+cd opencv-3.1.0
+
 cmake -G 'Unix Makefiles' \
   -DCMAKE_VERBOSE_MAKEFILE=ON \
   -DCMAKE_BUILD_TYPE=Release  \
