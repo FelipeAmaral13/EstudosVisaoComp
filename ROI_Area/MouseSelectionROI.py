e928d31a6ae7625d42f363c2c620a766660cd78b
felipeSAmaral <felipesa@uninove.br>
2020-09-09 21:26:56 -0300

    Arrumando roi

--- ROI_Area/MouseSelectionROI.py
+++ ROI_Area/MouseSelectionROI.py
@@ -8,26 +8,26 @@ image_to_show = np.copy(image)
 
 #Estados iniciais do mouse
 mouse_pressed = False
-s_x = s_y = e_x = e_y = -1
+x_init = y_init = top_left_pt = bottom_right_pt = -1
 
 
 def mouse_callback(event, x, y, flags, param):
-    global image_to_show, s_x, s_y, e_x, e_y, mouse_pressed
+    global image_to_show, x_init, y_init, top_left_pt, bottom_right_pt, mouse_pressed
 
     if event == cv2.EVENT_LBUTTONDOWN:
         mouse_pressed = True
-        s_x, s_y = x, y
+        x_init, y_init = x, y
         image_to_show = np.copy(image)
 
     elif event == cv2.EVENT_MOUSEMOVE:
         if mouse_pressed:
             image_to_show = np.copy(image)
-            cv2.rectangle(image_to_show, (s_x, s_y),
+            cv2.rectangle(image_to_show, (x_init, y_init),
                           (x, y), (0, 255, 0), 1)
 
     elif event == cv2.EVENT_LBUTTONUP:
         mouse_pressed = False
-        e_x, e_y = x, y
+        top_left_pt, bottom_right_pt = x, y
 
 cv2.namedWindow('image')
 cv2.setMouseCallback('image', mouse_callback)
@@ -37,13 +37,13 @@ while True:
     k = cv2.waitKey(1)
 
     if k == ord('c'):
-        if s_y > e_y:
-            s_y, e_y = e_y, s_y
-        if s_x > e_x:
-            s_x, e_x = e_x, s_x
+        if y_init > bottom_right_pt:
+            y_init, bottom_right_pt = bottom_right_pt, y_init
+        if x_init > top_left_pt:
+            x_init, top_left_pt = top_left_pt, x_init
 
-        if e_y - s_y > 1 and e_x - s_x > 0:
-            image = image[s_y:e_y, s_x:e_x]
+        if bottom_right_pt - y_init > 1 and top_left_pt - x_init > 0:
+            image = image[y_init:bottom_right_pt, x_init:top_left_pt]
             image_to_show = np.copy(image)
     elif k == 27:
         break
