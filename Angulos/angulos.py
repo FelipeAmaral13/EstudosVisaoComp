f06067a0a2f3f7348e008b6be07dfc688a63bc48
felipeSAmaral <felipesa@uninove.br>
2020-09-30 20:32:49 -0300

    Melhorando os angulos

--- Angulos/angulos.py
+++ Angulos/angulos.py
@@ -1,13 +1,18 @@
 import cv2
 import math
+import numpy as np
+import copy
+
  
 path = '1.png'
 img = cv2.imread(path)
+img_copy = copy.copy(img)
 pointsList = []
  
 def mousePoints(event,x,y,flags,params):
     if event == cv2.EVENT_LBUTTONDOWN:
         size = len(pointsList)
+        print(size)
         if size != 0 and  size % 3 != 0 :
             cv2.line(img, tuple(pointsList[round((size-1)/3)*3]), (x, y), (0,255,0), 2)
         cv2.circle(img,(x,y),5,(0,0,255),cv2.FILLED)
@@ -25,13 +30,14 @@ def getAngle(pointsList):
     '''
     Função para pegar o angulo a partir de três pontos
     '''
+
     pt1, pt2, pt3 = pointsList[-3:]
     m1 = gradient(pt1, pt2)
     m2 = gradient(pt1, pt3)
     angR = math.atan((m2-m1)/(1 + (m2*m1))) # Angulos em Radianos
     angD = round(math.degrees(angR)) # Angulos em Graus
-    print(angD)
-    cv2.putText(img,str(angD),(pt1[0]-40,pt1[1]-20),cv2.FONT_HERSHEY_COMPLEX,
+    #print(angD)
+    cv2.putText(img,str(angD),(pt1[0],pt1[1]),cv2.FONT_HERSHEY_COMPLEX,
                 1.5,(0,0,255),2)
 
 while True: 
@@ -42,6 +48,13 @@ while True:
 
     cv2.imshow('Image',img)
     cv2.setMouseCallback('Image',mousePoints)
+
+    if cv2.waitKey(1) == ord('n'):
+        img = cv2.imread(path)       
+        cv2.imshow('Image', img)
+
+
+
     if cv2.waitKey(1) == 27:
         pointsList = []
         img = cv2.imread(path)
