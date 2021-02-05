b6fd1210a5c81db2795ffd4542dfa4f3ed7b6d51
felipesa <felipesa@uninove.br>
2021-02-04 21:38:30 -0300

    Classificacao de numeros escritos a mao

---
+++ Classificacao de Numeros/Processamento_img.py
@@ -0,0 +1,95 @@
+#! python3
+
+# Bibliotecas
+import cv2
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import imutils
+from imutils import contours
+
+# Caminho
+path = os.getcwd()
+
+def verificar_pasta(caminho):
+    # Verificar se pasta Repositorio existe
+    if os.path.isdir(caminho) == False:
+        print(f'A pasta {caminho} não existe. Criando diretório.')
+        os.mkdir(caminho)
+    else:
+        print(f'A pasta {caminho} existe.')
+
+verificar_pasta(path + r"\Repositorio")
+
+
+def deletar_txt(caminho):
+    '''Funcao para remover os arquivos txt existentes.
+        entrada: caminho dos arquivos que serao removidos'''
+    filelist = [ f for f in os.listdir(caminho) if f.endswith(".png") ]
+    for f in filelist:
+        os.remove(os.path.join(caminho , f))
+
+deletar_txt(path + r"\Repositorio")
+
+# Imagem com os dados
+img = cv2.imread(path + r'\Teste.png')
+
+# Pre-Processamento
+# Imagem em tons de cinza
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Filtro de suavizacao
+img_blur = cv2.medianBlur(gray, 3)
+
+# Detectcao de bordas
+edges = cv2.Canny(img_blur, 50, 255)
+
+# Morfologia matematica
+kernel = np.ones((9,9), np.uint8)
+img_dilation = cv2.dilate(edges, kernel, iterations=1) 
+
+# Segementacao
+ret,thresh1 = cv2.threshold(img_dilation,127,255,cv2.THRESH_BINARY)
+
+
+titles = ['Imagem Original','Tons de Cinza','Filtro Blur','Deteccao de Bordas','Dilatacao','Segmentacao']
+images = [img, gray, img_blur, edges, img_dilation, thresh1]
+
+
+for i in range(6):
+    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray',vmin=0,vmax=255)
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
+
+
+# Localizar contornos
+# cv2.RETR_TREE -> recupera todos os contornos e reconstrói uma hierarquia completa de contornos aninhados.
+# cv2.CHAIN_APPROX_SIMPLE -> Armazena apenas os pontos iniciais e finais dos contornos detectados.
+cnts = cv2.findContours(img_dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+# Ordenacao dos contornos encontrados
+(cnts, _) = imutils.contours.sort_contours(cnts, method='left-to-right')
+
+ROI_number = 0
+
+for c in cnts:
+    # Calcular a area encontrada nos contornos. Se area maior que 1100 provavelmente e um numero
+    area = cv2.contourArea(c)
+    if area > 1100 :
+        print(area)
+        x,y,w,h = cv2.boundingRect(c)  # Coordenadas dos contornos      
+        cv2.rectangle(img, (x - 1, y - 1), (x + 1 + w, y + 1 + h), (0, 0, 255), 1) # Selecionar um retangulo
+
+        ROI = thresh1[y:y+h, x:x+w]
+        ROI = cv2.resize(ROI, (28,28), interpolation = cv2.INTER_AREA) # Salvar a imagem 28x28 (Modelo treinado MNIST)
+        cv2.drawContours(img, [c], -1, (0,255,0), -1) # Desenhar o contorno localizado
+
+        cv2.imwrite(path + f'\\Repositorio\\img_{ROI_number}.png', ROI)
+        ROI_number += 1        
+
+        cv2.imshow('Digitos encontrados!', img)
+        key = cv2.waitKey(0)
+        if key == 27:  # (escape to quit)
+            cv2.destroyAllWindows()
---
+++ Classificacao de Numeros/Train_mnist_keras.py
@@ -0,0 +1,78 @@
+#! python3
+
+# Bilioteca
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from matplotlib import pyplot
+import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+
+
+# Model / data parameters
+num_classes = 10
+input_shape = (28, 28, 1)
+
+# Data, split 
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+print('Train: X=%s, y=%s' % (x_train.shape, y_train.shape))
+print('Test: X=%s, y=%s' % (x_test.shape, y_test.shape))
+
+# plotar 9 primeiras imagens
+for i in range(9):    
+    plt.subplot(330 + 1 + i)
+    plt.imshow(x_train[i], cmap=pyplot.get_cmap('gray'))
+plt.show()
+
+
+# resizing e normalizacao da imagem
+img_rows, img_cols = 28, 28
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1).astype("float32")
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1).astype("float32")
+x_train /= 255
+x_test /= 255
+
+
+# Modelo RNA
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+     activation='relu',
+     input_shape=(img_rows, img_cols, 1)))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss='sparse_categorical_crossentropy',
+      optimizer='adam',
+      metrics=['accuracy'])
+
+# Treinamento
+batch_size = 128
+epochs = 10
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+
+# Avaliacao simples do modelo
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# Salvar o modelo
+model.save("test_model.h5")
+
