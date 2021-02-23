62361f4fa05dd918249fc3249f5d91baecfa3534
FelipeAmaral13 <felipengmec@gmail.com>
2021-02-23 09:57:50 -0300

    Cod organizado

--- Cartoon Image/cartoonImage.py
+++ Cartoon Image/cartoonImage.py
@@ -1,28 +1,37 @@
 import cv2
 import numpy as np  
+import matplotlib.pyplot as plt
 
 
 img_rgb = cv2.imread('por_sol.png')
 print(img_rgb.shape)
 
-img_rgb = cv2.resize(img_rgb, (800, 800))
+image = cv2.resize(img_rgb, (800, 800))
 
-image = img_rgb
+#Imagem em escala de cinza
+grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-#convert to gray scale
-grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)#apply gaussian blur
-grayImage = cv2.GaussianBlur(grayImage, (3, 3), 0)#detect edges
+#Filtro gaussianBlur
+grayImage = cv2.GaussianBlur(grayImage, (3, 3), 0)
+
+#Deteccao de bordas
 edgeImage = cv2.Laplacian(grayImage, -1, ksize=5)
-edgeImage = 255 - edgeImage#threshold image
-ret, edgeImage = cv2.threshold(edgeImage, 150, 255, cv2.THRESH_BINARY)#blur images heavily using edgePreservingFilter
-edgePreservingImage = cv2.edgePreservingFilter(image, flags=2, sigma_s=50, sigma_r=0.4)#create output matrix
-output =np.zeros(grayImage.shape)#combine cartoon image and edges image
-output = cv2.bitwise_and(edgePreservingImage, edgePreservingImage, mask=edgeImage)#Visualize the cartoon image 
-cv2.imshow("Cartoon", output) 
-cv2.waitKey(0) # "0" is Used to close the image window
-cv2.destroyAllWindows()
+edgeImage = 255 - edgeImage
 
-cartoon_image = cv2.stylization(image, sigma_s=150, sigma_r=0.25)  
+#threshold 
+ret, edgeImage = cv2.threshold(edgeImage, 150, 255, cv2.THRESH_BINARY)
+
+#Desforque da imagem usando a funcao edgePreservingFilter
+edgePreservingImage = cv2.edgePreservingFilter(image, flags=2, sigma_s=50, sigma_r=0.4)
+
+#Criando uma matriz de saida
+output = np.zeros(grayImage.shape)
+
+#Combinando a imagem cartoon e a imagem com bordas detectadas
+output = cv2.bitwise_and(edgePreservingImage, edgePreservingImage, mask=edgeImage)
+
+cartoon_image = cv2.stylization(output, sigma_s=150, sigma_r=0.25)  
+
 cv2.imshow('cartoon', cartoon_image)  
 cv2.waitKey(0)  
 cv2.destroyAllWindows()
