6df55f8f1daa517193c7274b994edb686d594b90
Felipe Souza Amaral <felipengmec@gmail.com>
2022-01-10 13:26:52 -0300

    Padranizando PEP8

--- Cartoon Image/cartoonImage.py
+++ Cartoon Image/cartoonImage.py
@@ -1,39 +1,39 @@
 import cv2
-import numpy as np  
-import matplotlib.pyplot as plt
+import numpy as np
+import os
 
-
-img_rgb = cv2.imread('por_sol.png')
+img_rgb = cv2.imread(os.path.join(os.getcwd(), 'por_sol.png'))
 print(img_rgb.shape)
 
 image = cv2.resize(img_rgb, (800, 800))
 
-#Imagem em escala de cinza
+# Imagem em escala de cinza
 grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-#Filtro gaussianBlur
+# Filtro gaussianBlur
 grayImage = cv2.GaussianBlur(grayImage, (3, 3), 0)
 
-#Deteccao de bordas
+# Deteccao de bordas
 edgeImage = cv2.Laplacian(grayImage, -1, ksize=5)
 edgeImage = 255 - edgeImage
 
-#threshold 
+# threshold
 ret, edgeImage = cv2.threshold(edgeImage, 150, 255, cv2.THRESH_BINARY)
 
-#Desforque da imagem usando a funcao edgePreservingFilter
-edgePreservingImage = cv2.edgePreservingFilter(image, flags=2, sigma_s=50, sigma_r=0.4)
+# Desforque da imagem usando a funcao edgePreservingFilter
+edgePreservingImage = cv2.edgePreservingFilter(
+    image, flags=2, sigma_s=50, sigma_r=0.4
+    )
 
-#Criando uma matriz de saida
+# Criando uma matriz de saida
 output = np.zeros(grayImage.shape)
 
-#Combinando a imagem cartoon e a imagem com bordas detectadas
-output = cv2.bitwise_and(edgePreservingImage, edgePreservingImage, mask=edgeImage)
+# Combinando a imagem cartoon e a imagem com bordas detectadas
+output = cv2.bitwise_and(
+    edgePreservingImage, edgePreservingImage, mask=edgeImage)
 
-cartoon_image = cv2.stylization(output, sigma_s=150, sigma_r=0.25)  
+cartoon_image = cv2.stylization(output, sigma_s=150, sigma_r=0.25)
 
-cv2.imshow('cartoon', cartoon_image)  
-cv2.waitKey(0)  
+cv2.imshow('cartoon', cartoon_image)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
--- Cartoon Image/webCam_cartoonImage.py
+++ Cartoon Image/webCam_cartoonImage.py
@@ -5,13 +5,14 @@ num_down = 2
 num_bilateral = 7
 
 
-def cartoon_img(img_rgb, frame):
+def cartoon_img(img_rgb: np.ndarray):
 
     # Downsampling da imagem usando Gaussian Pyramid
     for _ in range(num_down):
         img_rgb = cv2.pyrDown(img_rgb)
 
-    # Aplicacao de um filtro bilateral. O filtro bilateral irá diminuir o pallete das cores, necessario para o efeito de cartoon
+    # Aplicacao de um filtro bilateral. O filtro bilateral irá diminuir
+    # o pallete das cores, necessario para o efeito de cartoon
     for _ in range(num_bilateral):
         img_rgb = cv2.bilateralFilter(img_rgb, d=9, sigmaColor=9, sigmaSpace=7)
 
@@ -27,16 +28,16 @@ def cartoon_img(img_rgb, frame):
 
     # Deteccao de bordas
     img_edge = cv2.adaptiveThreshold(
-        img_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, blockSize=9, C=2)
+        img_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+        cv2.THRESH_BINARY, blockSize=9, C=2)
 
     # Conversao da imagem para RGB
     img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
 
     # Combinando a imagem colorida com a imagem com bordas destacadas
-    img_cartoon = cv2.bitwise_and(img_rgb, img_edge)
+    # img_cartoon = cv2.bitwise_and(img_rgb, img_edge)
+    # stack = np.hstack([img_rgb, img_cartoon])
 
-    #stack = np.hstack([img_rgb, img_cartoon])
-    
     return img_rgb
 
 
@@ -48,7 +49,7 @@ while True:
 
     img_rgb = cv2.resize(frame, (800, 800))
 
-    img = cartoon_img(img_rgb, frame)
+    img = cartoon_img(img_rgb)
     cv2.imshow("Cartoon", img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
