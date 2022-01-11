7bf7157172cb4f45795d9ab22e039c987e8ee884
Felipe Souza Amaral <felipengmec@gmail.com>
2022-01-10 21:48:34 -0300

    Padronizando  PEP8

--- ObjectDetectionTracking/HSV.py
+++ ObjectDetectionTracking/HSV.py
@@ -1,48 +1,49 @@
 import cv2
 import numpy as np
 
-
 cap = cv2.VideoCapture('2.mp4')
+
 
 def nothing(x):
     pass
+
+
 cv2.namedWindow('Resultado')
 
 # Começando com 100 para prevenir error da mask
-h,s,v = 100,100,100
+h, s, v = 100, 100, 100
 
 # Creating track bar
-cv2.createTrackbar('h', 'Resultado',0,179,nothing)
-cv2.createTrackbar('s', 'Resultado',0,255,nothing)
-cv2.createTrackbar('v', 'Resultado',0,255,nothing)
+cv2.createTrackbar('h', 'Resultado', 0, 179, nothing)
+cv2.createTrackbar('s', 'Resultado', 0, 255, nothing)
+cv2.createTrackbar('v', 'Resultado', 0, 255, nothing)
 
 while True:
 
     ret, frame = cap.read()
 
-    #Convertando para HSV
-    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    # Convertando para HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # Pegando as informacoes 
-    h = cv2.getTrackbarPos('h','Resultado')
-    s = cv2.getTrackbarPos('s','Resultado')
-    v = cv2.getTrackbarPos('v','Resultado')
+    # Pegando as informacoes
+    h = cv2.getTrackbarPos('h', 'Resultado')
+    s = cv2.getTrackbarPos('s', 'Resultado')
+    v = cv2.getTrackbarPos('v', 'Resultado')
 
     # Mascara
-    lower_blue = np.array([h,s,v])
-    upper_blue = np.array([180,255,255])
+    lower_blue = np.array([h, s, v])
+    upper_blue = np.array([180, 255, 255])
 
-    #Aplicando a mascara
-    mask = cv2.inRange(hsv,lower_blue, upper_blue)
+    # Aplicando a mascara
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
-    result = cv2.bitwise_and(frame,frame,mask = mask)
+    result = cv2.bitwise_and(frame, frame, mask=mask)
 
-    cv2.imshow('Resultado',result)
+    cv2.imshow('Resultado', result)
 
     k = cv2.waitKey(30) & 0xFF
     if k == 27:
         break
 
 cap.release()
-
 cv2.destroyAllWindows()
--- ObjectDetectionTracking/objDetectionTrack.py
+++ ObjectDetectionTracking/objDetectionTrack.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2
 
-cor = np.uint8([[[0,255,0]]])
-hsv_cor  = cv2.cvtColor(cor, cv2.COLOR_BGR2HSV)
+cor = np.uint8([[[0, 255, 0]]])
+hsv_cor = cv2.cvtColor(cor, cv2.COLOR_BGR2HSV)
 print(hsv_cor)
 
 cap = cv2.VideoCapture(0)
@@ -11,22 +11,24 @@ while True:
     ret, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    #Limites superior e inferior
-    lim_inf = np.array([100,100,100])
-    lim_sup = np.array([140,255,255])
+    # Limites superior e inferior
+    lim_inf = np.array([100, 100, 100])
+    lim_sup = np.array([140, 255, 255])
 
-    #Aplicacao dos lim
-    color_mask = cv2.inRange(hsv, lim_inf, lim_sup) #Verifica se os elementos da matriz estão entre os elementos de duas outras matrizes
+    # Aplicacao dos lim
+    # Verifica se os elementos da matriz estão entre os
+    # elementos de duas outras matrizes
+    color_mask = cv2.inRange(hsv, lim_inf, lim_sup)
 
-    (couts,hir) = cv2.findContours(color_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+    (couts, hir) = cv2.findContours(
+        color_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     for cout in couts:
         area = cv2.contourArea(cout)
 
         if (area > 800):
-            x,y,w,h = cv2.boundingRect(cout)
-            frame = cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+            x, y, w, h = cv2.boundingRect(cout)
+            frame = cv2.rectangle(frame, (x,  y), (x+w, y+h), (0, 0, 255), 2)
 
     cv2.imshow('ObjectDetctionTrack', frame)
 
