672928fa14cb64fadea5295babc28c669977db22
Felipe <felipengmec@gmail.com>
2023-03-29 18:57:57 -0300

    Refatorando

--- Color Segmetation/ColorSegmentationWebcam.py
+++ Color Segmetation/ColorSegmentationWebcam.py
@@ -1,36 +1,30 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+class CameraCapture:
+    
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+        
+    def range_HSV(self, frame, H: int, S: int, V: int):
+        red_low = np.array([H, S, V])
+        red_high = np.array([255, 255, 255])
+        red_mask = cv2.inRange(frame, red_low, red_high)
+        red = cv2.bitwise_and(frame, frame, mask=red_mask)
+        return red
 
+    def start_capture(self):
+        while True:
+            ret, frame = self.cap.read()
+            hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            color_hsv = self.range_HSV(hsv_frame, 160, 115, 85)
+            cv2.imshow("RED", color_hsv)
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
+        cv2.destroyAllWindows()
+        self.cap.release()
 
-def range_HSV(H: int, S: int, V: int):
-
-    red_low = np.array([H, S, V])
-    red_high = np.array([255, 255, 255])
-    red_mask = cv2.inRange(frame, red_low, red_high)
-    red = cv2.bitwise_and(frame, frame, mask=red_mask)
-
-    return red
-
-
-while True:
-
-    ret, frame = cap.read()
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    color_hsv = range_HSV(160, 115, 85)
-
-    # cv2.imshow("Frame", frame)
-    # cv2.imshow("Frame", result)
-    cv2.imshow("RED", color_hsv)
-    # cv2.imshow('Blue_HSV', blue_frame)
-
-    key = cv2.waitKey(1)
-
-    if key == 27:
-        break
-
-
-cv2.destroyAllWindows()
-cap.release()
+if __name__ == '__main__':
+    cap = CameraCapture()
+    cap.start_capture()
--- Color Segmetation/README.md
+++ Color Segmetation/README.md
@@ -1,4 +1,40 @@
-# Color Segmentation
+# Detecção de cores em tempo real usando OpenCV
 
-Esse programa tem como objetivo fazer a segmentação de cores através do espaço de cores HSV.
+Este é um projeto simples que demonstra como detectar uma cor específica em tempo real usando a biblioteca OpenCV. O código é escrito em Python e encapsulado em uma classe para tornar mais fácil o uso e a manutenção.
+# Pré-requisitos
 
+Antes de executar o código, é necessário ter os seguintes itens instalados:
+
+    Python 3.x
+    OpenCV
+    Numpy
+
+# Instalação
+
+    Clone este repositório para sua máquina local:
+
+
+
+git clone https://github.com/seu-usuario/detector-cores-opencv.git
+
+    Certifique-se de que você tem os pré-requisitos instalados.
+    Execute o seguinte comando para iniciar o aplicativo:
+
+
+
+`python main.py`
+
+# Como usar
+
+A classe CameraCapture encapsula toda a lógica de captura de vídeo e detecção de cor. Se você deseja usar em outro projeto, siga estas etapas:
+
+    Copie a classe CameraCapture para seu projeto.
+    Crie uma instância da classe CameraCapture.
+    Chame o método start_capture() para iniciar a captura de vídeo.
+
+# Créditos
+
+Este projeto foi desenvolvido por Seu nome aqui.
+# Licença
+
+Este projeto é licenciado sob a licença MIT - veja o arquivo LICENSE para detalhes.
