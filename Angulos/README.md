9a4f312924fdeb48fe2446df601d1ccfca470457
Felipe <felipengmec@gmail.com>
2023-03-29 17:33:56 -0300

    refatorando o codigo principal

--- Angulos/README.md
+++ Angulos/README.md
@@ -1,8 +1,39 @@
-# Calculo de ângulos entre duas semiretas
+# Detecção de ângulos em uma imagem
 
-Este programa tem como objetivo calcular o ângulo entre duas semiretas que foram marcadas através do mouse.
-Depois de marcados todos os ângulos é possível resetar a imagem e refazer as marcações novamente
+Este código permite a detecção de ângulos em uma imagem. É possível marcar pontos na imagem com o mouse, e o código calcula os ângulos entre as retas formadas pelos pontos. Os resultados são exibidos na própria imagem.
 
+# Pré-requisitos
+
+    Python 3
+    Bibliotecas OpenCV e Numpy
+
+# Instalação
+
+Para instalar as bibliotecas necessárias, execute o seguinte comando:
+
+pip install opencv-python numpy
+
+# Uso
+
+Para executar o código, basta rodar o arquivo main.py na linha de comando:
+
+`python main.py --image_path <path/to/image>`
+
+Substitua <path/to/image> pelo caminho para a imagem que você deseja analisar.
+
+Durante a execução do programa, abra a janela da imagem e clique em três pontos para formar um triângulo. O programa então calcula o ângulo entre as duas retas formadas pelos pontos.
+
+Pressione n para resetar a imagem e começar novamente.
+
+Pressione ESC para sair do programa.
+
+# Contribuições
+
+Contribuições são bem-vindas! Sinta-se à vontade para abrir uma issue ou um pull request para sugerir melhorias ou corrigir bugs.
+
+# Licença
+
+Este código é distribuído sob a licença MIT. Consulte o arquivo LICENSE para mais informações.
 
 ### Exemplo da aplicação
 ![1](https://user-images.githubusercontent.com/5797933/96590456-6e620000-12bc-11eb-8a24-9aa8a6684292.jpeg)
--- Angulos/angulos.py
+++ Angulos/angulos.py
@@ -1,72 +1,88 @@
 import cv2
 import math
-import copy
 import os
-
-path = os.path.join(os.getcwd(), 'Imagens', '2.png')
-img = cv2.imread(path)
-img_copy = copy.copy(img)
-pointsList = []
+import argparse
 
 
-def mousePoints(event: int, x: int, y: int, flags, params):
+def parse_args():
+    parser = argparse.ArgumentParser(description='Calculates angle between lines in an image')
+    parser.add_argument('--image_path', type=str, help='Path to image file')
+    return parser.parse_args()
+
+
+def validate_image_path(image_path):
+    if not os.path.exists(image_path):
+        raise ValueError(f'Image file "{image_path}" not found')
+
+
+def mouse_points(event, x, y, flags, params):
     '''
     Função para captação dos clicks do mouse.
     '''
     if event == cv2.EVENT_LBUTTONDOWN:
-        size = len(pointsList)
-        print(size)
+        size = len(params['points_list'])
         if size != 0 and size % 3 != 0:
             cv2.line(
-                img,
-                tuple(pointsList[round((size-1)/3)*3]), (x, y), (0, 255, 0), 2)
-        cv2.circle(img, (x, y), 5, (0, 0, 255), cv2.FILLED)
-        pointsList.append([x, y])
+                params['img'],
+                tuple(params['points_list'][round((size-1)/3)*3]), (x, y), (0, 255, 0), 2)
+        cv2.circle(params['img'], (x, y), 5, (0, 0, 255), cv2.FILLED)
+        params['points_list'].append([x, y])
 
 
-def slope(pt1: int, pt2: int):  # Slope das retas
+def slope(pt1, pt2):  # Slope das retas
     '''
     Função para pegar o slope, ou também conhecido como gradiente, das retas
     '''
     return (pt2[1] - pt1[1])/(pt2[0] - pt1[0])
 
 
-def getAngle(pointsList: list):
+def get_angle(points_list, img):
     '''
     Função para pegar o angulo a partir de três pontos
     '''
-    pt1, pt2, pt3 = pointsList[-3:]
+    if len(points_list) < 3:
+        print('Not enough points selected')
+        return
+
+    pt1, pt2, pt3 = points_list[-3:]
     m1 = slope(pt1, pt2)
     m2 = slope(pt1, pt3)
     angR = math.atan((m2-m1)/(1 + (m2*m1)))  # Angulos em Radianos
     angD = round(math.degrees(angR))  # Angulos em Grau
     if angD < 0:
-        angD = angD + 360
-    else:
-        angD
+        angD += 360
 
-    cv2.putText(img, str(angD), (pt1[0], pt1[1] + 10),
+    cv2.putText(img, str(angD), tuple(pt1),
                 cv2.FONT_HERSHEY_COMPLEX, 1.5, (0, 0, 255), 2)
 
 
-while True:
+def main(image_path):
+    validate_image_path(image_path)
 
-    if len(pointsList) % 3 == 0 and len(pointsList) != 0:
-        getAngle(pointsList)
+    img = cv2.imread(image_path)
+    img_copy = img.copy()
+    points_list = []
 
-    cv2.imshow('Image', img)
-    cv2.setMouseCallback('Image', mousePoints)
+    while True:
+        if len(points_list) % 3 == 0 and len(points_list) != 0:
+            get_angle(points_list, img)
 
-    # Reset da imagem. Apagar todos os textos inseridos na imagem
-    if cv2.waitKey(1) == ord('n'):
-        img = cv2.imread(path)
-        pointsList = []
         cv2.imshow('Image', img)
+        cv2.setMouseCallback('Image', mouse_points, {'img': img, 'points_list': points_list})
 
-    # Apertar 'ESC' para sair
-    if cv2.waitKey(1) == 27:
-        pointsList = []
-        img = cv2.imread(path)
-        break
+        # Reset da imagem. Apagar todos os textos inseridos na imagem
+        if cv2.waitKey(1) == ord('n'):
+            img = img_copy.copy()
+            points_list = []
+            cv2.imshow('Image', img)
 
-cv2.destroyAllWindows()
+        # Apertar 'ESC' para sair
+        if cv2.waitKey(1) == 27:
+            points_list = []
+            cv2.destroyAllWindows()
+            break
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args.image_path)
