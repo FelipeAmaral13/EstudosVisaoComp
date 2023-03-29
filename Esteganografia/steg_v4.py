0e6b5446ee4569c405222e3fa43571b95bc0e8d5
Felipe <felipengmec@gmail.com>
2023-03-29 19:51:24 -0300

    Add class in this project

--- Esteganografia/steg_v4.py
+++ Esteganografia/steg_v4.py
@@ -3,72 +3,43 @@ import numpy as np
 from PIL import Image
 from pyzbar.pyzbar import decode
 
+class QrCode:
+    def __init__(self, message):
+        self.img_qrcode = qrcode.make(message)
+        self.img_encoded = None
+    
+    def encode(self, img_real):
+        img_oficial = Image.open(img_real).convert('L')
+        secret = self.img_qrcode.resize(img_oficial.size)
+        data_c = np.array(img_oficial)
+        data_s = np.array(secret, dtype=np.uint8)
+        res = data_c & ~1 | data_s
+        self.img_encoded = Image.fromarray(res).convert("L")
+    
+    def decode(self):
+        data_s = np.array(self.img_encoded)
+        data_s = data_s & 1
+        img_qr = Image.fromarray(data_s * np.uint(255))
+        return img_qr
+    
+    def get_text(self):
+        result = decode(self.decode())
+        print(result[0][0].decode('utf-8'))
+    
+    def plot_bitplanes(self):
+        data = np.array(self.img_encoded)
+        out = []
 
-# Criar o a mensagem do qrcode
-def qr_text(texto: str) -> Image.Image:
-    return qrcode.make(texto)
+        for k in range(7, -1, -1):
+            res = data // 2**k & 1
+            out.append(res*255)
 
-
-msg_secret = qr_text('Imagem para teste')
-
-
-def encode(img_real: str, img_qrcode: qrcode.image.pil.PilImage):
-
-    img_oficial = Image.open(img_real).convert('L')
-
-    # Trocar a alt e lar da imagem que vai ser inserida na imgem oficial
-    secret = img_qrcode.resize(img_oficial.size)
-
-    # Formatacao da imagem
-    data_c = np.array(img_oficial)
-    data_s = np.array(secret, dtype=np.uint8)
-
-    # Colocar as informaçoes no bits menos signficativos
-    res = data_c & ~1 | data_s
-
-    new_img = Image.fromarray(res).convert("L")
-    return new_img
-
-
-new_img = encode('lena.jpg', msg_secret)
-
-
-def decode(img_encode: Image.Image):
-
-    data_s = np.array(img_encode)
-    # Pegar as informacoes dos bits menos significativos
-    data_s = data_s & 1
-    new_img = Image.fromarray(data_s * np.uint(255))
-
-    return new_img
+        b = np.hstack(out)
+        Image.fromarray(b).show()
 
 
-img_qr = decode(new_img)
 
-
-def get_text(imagem: Image.Image):
-
-    result = decode(imagem)
-    print(result[0][0].decode('utf-8'))
-
-
-get_text(img_qr)
-
-
-def bitplanes(im: Image.Image):
-
-    data = np.array(im)
-    out = []
-
-    # Crar uma img para cada  plato de k bits
-    for k in range(7, -1, -1):
-        # Extrair o Kth bit (de 0 a 7)
-        res = data // 2**k & 1
-        out.append(res*255)
-
-    # empilhar a geracao de imagens
-    b = np.hstack(out)
-    return Image.fromarray(b)
-
-
-bitplanes(new_img).show()
+qrcode = QrCode('exemplo de mensagem')
+qrcode.encode('lena.jpg')
+qrcode.plot_bitplanes()
+qrcode.get_text()
