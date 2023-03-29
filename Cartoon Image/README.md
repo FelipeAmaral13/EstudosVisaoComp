e2999284b1bbd7e23bce4702ad88739c039c0eb4
Felipe <felipengmec@gmail.com>
2023-03-29 18:07:21 -0300

    Create a new class webcam

--- Cartoon Image/README.md
+++ Cartoon Image/README.md
@@ -1,5 +1,53 @@
-# Projeto - Cartoon Filter
+# Cartoonizer
 
-Projeto com  afinalidade de criar um filtro do tipo cartoon tanto para imagens quanto para webcam
+A simple class to cartoonize an input image. The class uses OpenCV, NumPy, Matplotlib, and pathlib Python libraries to process the image.
 
+# Dependencies
+
+    Python 3.x
+    OpenCV
+    NumPy
+    Matplotlib
+
+# Installation
+
+Use pip to install the required libraries:
+
+pip install opencv-python numpy matplotlib
+
+# Usage
+
+To use the Cartoonizer class, create an instance of the class with the input image path as an argument. Then, apply the image processing methods to generate a cartoonized image. Finally, call the show_steps() method to display the steps of the cartoonization process and the final result.
+
+Example:
+
+from Cartoonizer import Cartoonizer
+
+if __name__ == "__main__":
+    cartoonizer = Cartoonizer('input_image.png')
+
+    cartoonizer.resize(800, 800)
+    cartoonizer.convert_to_gray()
+    cartoonizer.apply_gaussian_blur((3, 3), 0)
+    cartoonizer.detect_edges(5, 150)
+    cartoonizer.edge_preserving_filter(50, 0.4)
+    cartoonizer.stylize_image(150, 0.25)
+
+    cartoonizer.show_steps()
+
+# Class methods
+
+    * __init__(self, input_path: str): Initializes the class with the input image path.
+    * resize(self, width: int, height: int): Resizes the image to the specified width and height.
+    * convert_to_gray(self): Converts the image to grayscale.
+    * apply_gaussian_blur(self, kernel_size: tuple, sigma: float): Applies a Gaussian blur filter to the image with the specified kernel size and sigma value.
+    * detect_edges(self, ksize: int, threshold: int): Detects the edges of the image using the Laplacian operator and a threshold value.
+    * edge_preserving_filter(self, sigma_s: int, sigma_r: float): Applies an edge-preserving filter to the image with the specified sigma_s and sigma_r values.
+    * stylize_image(self, sigma_s: int, sigma_r: float): Stylizes the image with the specified sigma_s and sigma_r values.
+    * show_image(self, img, title): Displays the specified image with the specified title.
+    * show_steps(self): Displays the steps of the cartoonization process and the final result.
+
+# License
+
+This project is licensed under the MIT License.
 ![Capturar2](https://user-images.githubusercontent.com/5797933/130156526-19b87594-62a7-4220-a739-d9c19951157f.PNG)
--- Cartoon Image/cartoonImage.py
+++ Cartoon Image/cartoonImage.py
@@ -1,39 +1,63 @@
 import cv2
 import numpy as np
-import os
+from pathlib import Path
+import matplotlib.pyplot as plt
 
-img_rgb = cv2.imread(os.path.join(os.getcwd(), 'por_sol.png'))
-print(img_rgb.shape)
 
-image = cv2.resize(img_rgb, (800, 800))
+import matplotlib.pyplot as plt
 
-# Imagem em escala de cinza
-grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+class Cartoonizer:
+    def __init__(self, input_path: str):
+        self.input_path = Path(input_path)
+        self.img_rgb = cv2.imread(str(self.input_path))
 
-# Filtro gaussianBlur
-grayImage = cv2.GaussianBlur(grayImage, (3, 3), 0)
+    def resize(self, width: int, height: int):
+        self.image = cv2.resize(self.img_rgb, (width, height))
 
-# Deteccao de bordas
-edgeImage = cv2.Laplacian(grayImage, -1, ksize=5)
-edgeImage = 255 - edgeImage
+    def convert_to_gray(self):
+        self.gray_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
 
-# threshold
-ret, edgeImage = cv2.threshold(edgeImage, 150, 255, cv2.THRESH_BINARY)
+    def apply_gaussian_blur(self, kernel_size: tuple, sigma: float):
+        self.gray_image = cv2.GaussianBlur(self.gray_image, kernel_size, sigma)
 
-# Desforque da imagem usando a funcao edgePreservingFilter
-edgePreservingImage = cv2.edgePreservingFilter(
-    image, flags=2, sigma_s=50, sigma_r=0.4
-    )
+    def detect_edges(self, ksize: int, threshold: int):
+        self.edge_image = cv2.Laplacian(self.gray_image, -1, ksize=ksize)
+        self.edge_image = 255 - self.edge_image
+        _, self.edge_image = cv2.threshold(self.edge_image, threshold, 255, cv2.THRESH_BINARY)
 
-# Criando uma matriz de saida
-output = np.zeros(grayImage.shape)
+    def edge_preserving_filter(self, sigma_s: int, sigma_r: float):
+        self.edge_preserving_image = cv2.edgePreservingFilter(self.image, flags=2, sigma_s=sigma_s, sigma_r=sigma_r)
 
-# Combinando a imagem cartoon e a imagem com bordas detectadas
-output = cv2.bitwise_and(
-    edgePreservingImage, edgePreservingImage, mask=edgeImage)
+    def stylize_image(self, sigma_s: int, sigma_r: float):
+        output = np.zeros(self.gray_image.shape)
+        output = cv2.bitwise_and(self.edge_preserving_image, self.edge_preserving_image, mask=self.edge_image)
+        self.cartoon_image = cv2.stylization(output, sigma_s=sigma_s, sigma_r=sigma_r)
 
-cartoon_image = cv2.stylization(output, sigma_s=150, sigma_r=0.25)
+    def show_image(self, img, title):
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        plt.title(title)
+        plt.axis('off')
+        plt.show()
 
-cv2.imshow('cartoon', cartoon_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    def show_steps(self):
+        self.show_image(self.img_rgb, 'Original')
+        self.show_image(self.image, 'Resized')
+        self.show_image(self.gray_image, 'Gray')
+        self.show_image(self.edge_image, 'Edges')
+        self.show_image(self.edge_preserving_image, 'Edge-Preserving Filter')
+        self.show_image(self.cartoon_image, 'Cartoon')
+
+
+
+
+if __name__ == "__main__":
+    cartoonizer = Cartoonizer('por_sol.png')
+
+    cartoonizer.resize(800, 800)
+    cartoonizer.convert_to_gray()
+    cartoonizer.apply_gaussian_blur((3, 3), 0)
+    cartoonizer.detect_edges(5, 150)
+    cartoonizer.edge_preserving_filter(50, 0.4)
+    cartoonizer.stylize_image(150, 0.25)
+
+    cartoonizer.show_steps()
